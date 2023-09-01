08ced5aad45e5a81a2ea65717c8b9b6e542f69b2
Kirill Misyuro <kmisyuro@gmail.com>
2023-09-01 23:01:01 +0300

    fix map markers, add dynamic table

--- CarSimulatorBS/wwwroot/js/mapScripts.js
+++ CarSimulatorBS/wwwroot/js/mapScripts.js
@@ -1,16 +1,23 @@
-﻿function createMap() {
+﻿var markers = [];
+
+function createMap() {
     map = L.map('map').setView([53.677839, 23.829529], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom:18,
+        maxZoom: 18,
         //minZoom:12
     }).addTo(map);
 }
 
 function addMarker(lat, lng, title) {
-    L.marker([lat, lng]).addTo(map).bindPopup(title);
+    var marker = L.marker([lat, lng]).addTo(map).bindPopup(title);
+    markers.push({lat, lng, title, layer: marker});
 }
-var popupContent = '<div><button class="marker-button">Button</button></div>';
-function test(){
-    return xer;
+
+function clearMarkers() {
+    markers.forEach(function(marker) {
+        map.removeLayer(marker.layer);
+    });
+    markers = [];
+    map.invalidateSize();
 }
